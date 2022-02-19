6c964ab7840c6c8a34c19bae426ede617b4f0c11
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-19 17:20:51 +0300

    completeTodo modified

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {GrFormClose, GrFormEdit, GrFormCheckmark} from "react-icons/gr";
 import { useTodoLayerValue } from '../context/TodoContext';
+import clsx from "clsx";
 
 const Todo = ({todo}) => {
 
+    const [editable, setEditTable] = useState(false)
+    const [content, setContent] = useState(todo.content)
     const [{}, dispatch] = useTodoLayerValue(); //bir action çağıracağız
     const removeTodo = (todoId) => {
       dispatch({
@@ -19,16 +22,28 @@ const Todo = ({todo}) => {
 
    })
  }
+ const todoStyle = clsx({//birleşmiş stiller dosyası
+    ["todo-row"]: true, //classNamei todo-row olana her zaman bu stilleri dahil et
+    ["completed"]: todo.isCompleted,
+ })
 
   return (
-    <div className='todo-row'>
+    <div className={todoStyle}> 
+      {/*completed kısmı app.css den geliyor oraya yazmıştık, üstü çizilicek,
+      bunu kullanabilmek için clsx küütphanesini indirdik.*/}
 
-      <div  onClick={()=> completeTodo(todo.id)}>{todo.content}</div>
+      <div  onClick={()=> completeTodo(todo.id)}>
+        {
+          editable ? 
+          (<input type="text" className="todo-input-edit" value={content} onChange = {(event => setContent(event.target.value))} />) 
+          : (todo.content)
+        }
+      </div>
       {/*tamamlandığında üstünü çizecek*/}
 
       <div className='todo-icons'>
         <GrFormClose className="todo-icon" onClick={()=> removeTodo(todo.id)} />
-        <GrFormEdit className="todo-icon" />
+        <GrFormEdit className="todo-icon" onClick={()=> setEditTable(true)} />
       </div>
     </div>
   )
