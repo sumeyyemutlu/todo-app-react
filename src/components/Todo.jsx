72a9d275b1a21f543c1b06c6bf80b669f427c400
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-19 15:11:02 +0300

    close and edit icons defined

---
+++ src/components/Todo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {GrFormClose, GrFormEdit, GrFormCheckmark} from "react-icons/gr";
+import { useTodoLayerValue } from '../context/TodoContext';
+
+const Todo = ({todo}) => {
+
+    const [{}, dispatch] = useTodoLayerValue(); //bir action çağıracağız
+    const removeTodo = todoId => {
+      dispatch({
+        type: "REMOVE_TODO",
+        payload: todoId,
+
+     })
+   }
+  return (
+    <div className='todo-row'>
+
+      <div>{todo.content}</div>
+
+      <div className='todo-icons'>
+        <GrFormClose className="todo-icon" onClick={()=> removeTodo(todo.id)} />
+        <GrFormEdit className="todo-icon" />
+      </div>
+    </div>
+  )
+}
+
+export default Todo;
+
