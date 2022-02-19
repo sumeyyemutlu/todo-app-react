18d912456d646032137a60493dda63588db40d7f
sumeyye mutlu <sumeyyemutlu11@gmail.com>
2022-02-19 16:24:33 +0300

    remove_todo reducer created

--- src/context/reducer.jsx
+++ src/context/reducer.jsx
@@ -1,24 +1,34 @@
 
 
  export const initialState = {
-    todo: []
+    todos: []
 }
 
-const reducer =(state, action) => {
-    console.log(action)
+const reducer = (state, action) => {
+    console.log(action);
 
     switch (action.type) {
         case "ADD_TODO":
-            
         return {
-            ...state
-        }
+           ...state,
+           todos: [action.payload, ...state.todos]
+           //böylece en son eklenen todo en yukarıda gösterilmiş oldu.
+        };
+        case "REMOVE_TODO":
+            return {
+               ...state,
+               todos: [...state.todos].filter(todo => todo.id !== action.payload)
+               //varsayılan statein kopyası üzerinden filter metodu ile
+               //gönderilen id ile statetteki id den farklı olanları listele dedik.
+               //bu sayede silme işlemi gerçekleştirildi.
+            };
+        
     
         default:
             return {
-                ...state
+                ...state,
             };
     }
 }
 
-export default reducer
+export default reducer;
